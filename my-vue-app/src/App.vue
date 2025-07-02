d1a3e5b06363234006a630d8de82a83df1c79e33
alsakib748 <alsakib748@gmail.com>
2025-07-02 23:43:03 +0600

    Import & Export Data

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,26 +1,17 @@
 <script setup>
 
-const messge = 'Welcome. Get ready to master Vue.js 3';
-
-const date = "30th June, 2025";
-
-function getDate(){
-  const date = new Date();
-  return date.toDateString();
-}
-
-const data = {
-  message: 'Welcome. Get ready to master Vue.js 3',
-  date: '30th June, 2025',
-}
+import {data} from './data/data.js';
 
 </script>
 
 <template>
       <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
-      <h1>{{ data.message }}</h1>
+      <h1 class="mt-10 text-xl text-gray-700">{{ data.message }}</h1>
       <h2 class="mt-10 text-xl text-gray-700">
         Today is <span>{{ data.date }}</span>
+      </h2>
+      <h2 class="mt-10 text-xl text-gray-700">
+        You have <span>{{ data.tasks[2] }} tasks for today</span>
       </h2>
     </div>
 </template>
---
+++ my-vue-app/src/data/data.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const messge = "Welcome. Get ready to master Vue.js 3";
+
+const date = "30th June, 2025";
+
+const data = {
+  message: "Welcome. Get ready to master Vue.js 3",
+  date: "30th June, 2025",
+  tasks: [1, 2, "No", 4, 5]
+};
+
+export { data };
+
+function getDate() {
+  const date = new Date();
+  return date.toDateString();
+}
