86c95a7e9aeaa760bc8509a69350512129044dae
alsakib748 <alsakib748@gmail.com>
2025-07-03 23:53:23 +0600

    Condition (If, Else If, Else Use)

--- my-vue-app6/src/App.vue
+++ my-vue-app6/src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+const location = "Dhaka";
+const weather = "s";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <section class="container mx-auto flex items-center flex-col">
+    <h1 class="text-2xl text-center py-10">Logic in Vue.js</h1>
+    <br />
+    <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
+      <h1 class="text-3xl">{{ location }}</h1>
+      <h2 class="text-2xl" v-if="weather == 's'">Wether: Sunny</h2>
+      <h2 class="text-2xl" v-else-if="weather == 'pc'">Wether: Partly Cloudy</h2>
+      <h2 class="text-2xl" v-else-if="weather == 'r'">Wether: Rainy</h2>
+      <h2 class="text-2xl" v-else-if="weather == 't'">Wether: Thunderstorm</h2>
+      <h2 class="text-2xl" v-else>Wether: Cyclone</h2>
+      <img v-if="weather == 's'" src="https://clipartcraft.com/images/sunny-clipart-rainy-5.png" alt=""
+        class="rounded-full w-96 h-96">
+      <img v-if="weather == 'pc'"
+        src="https://static.vecteezy.com/system/resources/previews/012/066/505/non_2x/sunny-and-rainy-day-weather-forecast-icon-meteorological-sign-3d-render-png.png"
+        alt="" class="rounded-full w-96 h-96">
+      <img v-if="weather == 'r'"
+        src="https://tse4.mm.bing.net/th/id/OIP.p2pyUauiaNTvdyI9Na6TNAAAAA?cb=thvnextc2&rs=1&pid=ImgDetMain&o=7&rm=3"
+        alt="" class="rounded-full w-96 h-96">
+      <img v-if="weather == 't'"
+        src="https://static.vecteezy.com/system/resources/thumbnails/027/994/880/small_2x/weather-report-3d-ui-icon-sun-and-rain-cloud-icon-png.png"
+        alt="" class="rounded-full w-96 h-96">
+    </div>
+  </section>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
