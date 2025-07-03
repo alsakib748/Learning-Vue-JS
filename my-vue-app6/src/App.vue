2bf6fa1c01365abaa278c68e621e9ef461bca48b
alsakib748 <alsakib748@gmail.com>
2025-07-04 00:21:43 +0600

    Group Condition

--- my-vue-app6/src/App.vue
+++ my-vue-app6/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 const location = "Dhaka";
-const weather = "pc";
+const weather = "t";
 </script>
 
 <template>
@@ -10,22 +10,38 @@ const weather = "pc";
     <br />
     <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
       <h1 class="text-3xl">{{ location }}</h1>
-      <h2 class="text-2xl" v-show="weather == 's'">Wether: Sunny</h2>
-      <h2 class="text-2xl" v-show="weather == 'pc'">Wether: Partly Cloudy</h2>
-      <h2 class="text-2xl" v-show="weather == 'r'">Wether: Rainy</h2>
-      <h2 class="text-2xl" v-show="weather == 't'">Wether: Thunderstorm</h2>
-      <h2 class="text-2xl" v-show="weather == 'c'">Wether: Cyclone</h2>
-      <img v-show="weather == 's'" src="https://clipartcraft.com/images/sunny-clipart-rainy-5.png" alt=""
-        class="rounded-full w-96 h-96">
-      <img v-show="weather == 'pc'"
-        src="https://static.vecteezy.com/system/resources/previews/012/066/505/non_2x/sunny-and-rainy-day-weather-forecast-icon-meteorological-sign-3d-render-png.png"
-        alt="" class="rounded-full w-96 h-96">
-      <img v-show="weather == 'r'"
-        src="https://tse4.mm.bing.net/th/id/OIP.p2pyUauiaNTvdyI9Na6TNAAAAA?cb=thvnextc2&rs=1&pid=ImgDetMain&o=7&rm=3"
-        alt="" class="rounded-full w-96 h-96">
-      <img v-show="weather == 't'"
-        src="https://static.vecteezy.com/system/resources/thumbnails/027/994/880/small_2x/weather-report-3d-ui-icon-sun-and-rain-cloud-icon-png.png"
-        alt="" class="rounded-full w-96 h-96">
+
+      <div v-if="weather == 's'" class="flex flex-col items-center justify-center">
+        <h2 class="text-2xl">Wether: Sunny</h2>
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/012/066/505/non_2x/sunny-and-rainy-day-weather-forecast-icon-meteorological-sign-3d-render-png.png"
+          alt="" class="rounded-full w-96 h-96">
+      </div>
+
+      <template v-if="weather == 'pc'">
+        <h2 class="text-2xl">Wether: Partly Cloudy</h2>
+        <img src="https://clipartcraft.com/images/sunny-clipart-rainy-5.png" alt="" class="rounded-full w-96 h-96">
+      </template>
+
+      <template v-if="weather == 'r'">
+        <h2 class="text-2xl">Wether: Rainy</h2>
+        <img
+          src="https://tse4.mm.bing.net/th/id/OIP.p2pyUauiaNTvdyI9Na6TNAAAAA?cb=thvnextc2&rs=1&pid=ImgDetMain&o=7&rm=3"
+          alt="" class="rounded-full w-96 h-96">
+      </template>
+
+      <template v-if="weather == 't'">
+        <h2 class="text-2xl">Wether: Thunderstorm</h2>
+
+        <img
+          src="https://static.vecteezy.com/system/resources/thumbnails/027/994/880/small_2x/weather-report-3d-ui-icon-sun-and-rain-cloud-icon-png.png"
+          alt="" class="rounded-full w-96 h-96">
+      </template>
+
+      <!-- todo: Note: Template is used to render multiple elements but it's not show on output . It's alternative of div -->
+
+      <h2 class="text-2xl" v-if="weather == 'c'">Wether: Cyclone</h2>
+
     </div>
   </section>
 
