00384797d2ca4cc409871dfebed7b584fc8c7f51
alsakib748 <alsakib748@gmail.com>
2025-07-04 00:47:04 +0600

    Diff: Between v-if & v-show

--- my-vue-app6/src/App.vue
+++ my-vue-app6/src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import { ref } from 'vue';
 const location = "Dhaka";
-const weather = "t";
+// const weather = "t";
+const weather = ref("s");
 </script>
 
 <template>
@@ -11,36 +13,49 @@ const weather = "t";
     <div class="w-1/3 bg-gray-200 p-10 flex items-center flex-col space-y-10">
       <h1 class="text-3xl">{{ location }}</h1>
 
-      <div v-if="weather == 's'" class="flex flex-col items-center justify-center">
-        <h2 class="text-2xl">Wether: Sunny</h2>
+      <!-- <div v-show="weather == 's'" class="flex flex-col items-center justify-center">
+        <h2 class="text-2xl" id="sunny">Wether: Sunny</h2>
         <img
           src="https://static.vecteezy.com/system/resources/previews/012/066/505/non_2x/sunny-and-rainy-day-weather-forecast-icon-meteorological-sign-3d-render-png.png"
           alt="" class="rounded-full w-96 h-96">
-      </div>
+        </div> -->
 
-      <template v-if="weather == 'pc'">
-        <h2 class="text-2xl">Wether: Partly Cloudy</h2>
-        <img src="https://clipartcraft.com/images/sunny-clipart-rainy-5.png" alt="" class="rounded-full w-96 h-96">
+
+      <template v-if="weather == 's'">
+        <h2 class="text-2xl" id="sunny">Wether: Sunny</h2>
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/012/066/505/non_2x/sunny-and-rainy-day-weather-forecast-icon-meteorological-sign-3d-render-png.png"
+          alt="" class="rounded-full w-96 h-96">
       </template>
 
-      <template v-if="weather == 'r'">
+      <!-- todo: Note: v-if render new every time. but v-show contain the events -->
+
+      <div v-show="weather == 'pc'">
+        <h2 class="text-2xl">Wether: Partly Cloudy</h2>
+        <img src="https://clipartcraft.com/images/sunny-clipart-rainy-5.png" alt="" class="rounded-full w-96 h-96">
+      </div>
+
+      <div v-show="weather == 'r'">
         <h2 class="text-2xl">Wether: Rainy</h2>
         <img
           src="https://tse4.mm.bing.net/th/id/OIP.p2pyUauiaNTvdyI9Na6TNAAAAA?cb=thvnextc2&rs=1&pid=ImgDetMain&o=7&rm=3"
           alt="" class="rounded-full w-96 h-96">
-      </template>
+      </div>
 
-      <template v-if="weather == 't'">
+      <div v-show="weather == 't'">
         <h2 class="text-2xl">Wether: Thunderstorm</h2>
 
         <img
           src="https://static.vecteezy.com/system/resources/thumbnails/027/994/880/small_2x/weather-report-3d-ui-icon-sun-and-rain-cloud-icon-png.png"
           alt="" class="rounded-full w-96 h-96">
-      </template>
+      </div>
 
       <!-- todo: Note: Template is used to render multiple elements but it's not show on output . It's alternative of div -->
 
       <h2 class="text-2xl" v-if="weather == 'c'">Wether: Cyclone</h2>
+
+      <input type="text" name="" placeholder="Weather Status
+      " class="text-center p-5 text-2xl" id="" v-model="weather" />
 
     </div>
   </section>
