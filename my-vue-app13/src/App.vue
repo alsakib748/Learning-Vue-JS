220fb749d4f4e5198c44498a770b7aaf06089a8a
alsakib748 <alsakib748@gmail.com>
2025-07-05 00:35:54 +0600

    Template use in loop

---
+++ my-vue-app13/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+const icons = [
+  {
+    title: "Sunny",
+    icon: "https://openweathermap.org/img/wn/01d@2x.png"
+  },
+
+  {
+    title: "Partly Cloudy",
+    icon: "https://openweathermap.org/img/wn/02d@2x.png"
+  },
+
+  {
+    title: "Rainy",
+    icon: "https://openweathermap.org/img/wn/10d@2x.png"
+  },
+
+  {
+    title: "Thunderstorm",
+    icon: "https://openweathermap.org/img/wn/11d@2x.png"
+  },
+
+];
+
+</script>
+
+<template>
+
+  <section class="mx-auto container">
+    <section class="mx-auto container space-y-3 p-5 border border-gray-500 w-80 flex flex-col items-center">
+      <template v-for="icon in icons" :key="icon.title" class="text-center">
+        <h2 class="text-2xl">{{ icon.title }}</h2>
+        <img :src="icon.icon" :alt="icon.title" class="w-16 h-16">
+      </template>
+    </section>
+  </section>
+
+</template>
+
+<style scoped></style>
