cf694afcab5e088787e344f3d33d604678bf5751
alsakib748 <alsakib748@gmail.com>
2025-07-04 23:06:36 +0600

    Task Show Using Loop

---
+++ my-vue-app10/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+];
+
+</script>
+
+<template>
+
+  <section class="">
+    <section class="">
+      <div class="mb-2 w-96 p-2 border border-gray-600 text-left" v-for="(task, index) in tasks">
+        {{ index + 1 }}. {{ task }}
+      </div>
+    </section>
+  </section>
+
+</template>
+
+<style scoped></style>
