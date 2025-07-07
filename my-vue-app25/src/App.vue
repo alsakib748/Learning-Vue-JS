dc9ca839e0e70d154dd575e2c439d1321a97e3ca
alsakib748 <alsakib748@gmail.com>
2025-07-07 23:09:42 +0600

    v-model use in radio & checkbox

---
+++ my-vue-app25/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+import { ref, reactive } from 'vue';
+
+const displayImage = ref(true);
+
+</script>
+
+<template>
+
+  <section class="mx-auto container">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="mb-10">{{ displayImage }}</p>
+    <label for="" class="">Display Random Image</label>
+    <input type="checkbox" class="ml-2" v-model="displayImage">
+    <p class="mt-5">
+      <input type="radio" name="display" v-model="displayImage" :value="true"> On <br />
+      <input type="radio" name="display" v-model="displayImage" :value="false"> Off <br />
+    </p>
+    <img v-show="displayImage"
+      :src="`https://picjumbo.com/wp-content/uploads/beautiful-nature-scenery-free-photo-2210x1473.jpg`"
+      class="mt-10 mx-auto w-[500px]">
+    <!-- <img v-show="displayImage" :src="`https://source.unsplash.com/random?version=${Math.random()}`"
+      class="mt-10 mx-auto w-[500px]"> -->
+  </section>
+
+</template>
+
+<style scoped></style>
