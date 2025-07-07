c1b669cfa23f04ebb05d1cd93c4cdde1fe806a82
alsakib748 <alsakib748@gmail.com>
2025-07-07 21:30:12 +0600

    Two Way Binding / v-model

---
+++ my-vue-app23/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+import { ref } from 'vue';
+
+const color = ref('None');
+
+function getStyle() {
+  return {
+    backgroundColor: color.value,
+  }
+}
+
+</script>
+
+<template>
+  <section class="mx-auto container">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="pb-5">Your Favorite Color: {{ color }}</p>
+    <p>
+      Your Favorite Color:
+      <input class="p-3 border border-purple-500" v-model="color" />
+    <div class="w-32 h-32 mt-10" :style="getStyle()"></div>
+    </p>
+  </section>
+</template>
+
+<style scoped></style>
