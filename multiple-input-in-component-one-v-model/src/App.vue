45a8c1c1ed22f5a3a0e8093610b8d9d8f73e16c3
alsakib748 <alsakib748@gmail.com>
2025-07-11 01:02:02 +0600

    Multiple input in component one v-model

---
+++ multiple-input-in-component-one-v-model/src/App.vue
@@ -0,0 +1,57 @@
+<script setup>
+
+import { ref, reactive } from 'vue';
+import PersonComponent from './components/PersonComponent.vue';
+import NameComponent from './components/NameComponent.vue';
+import NameComponentX from './components/NameComponentX.vue';
+
+const person = reactive({
+    name: 'John Doe',
+    email: 'john.doe@example.com'
+});
+
+const fname = ref('John');
+const lname = ref('Doe');
+
+</script>
+
+<template>
+
+    <div class="">
+
+        <div class="">
+            <img src="./assets/vue.svg" alt="" class="">
+            <h1 class="text-2xl">Component Model</h1>
+        </div>
+
+        <div class="">
+            Original Name: {{ person.name }} <br />
+        </div>
+
+        <div class="">
+            <label for="">Component</label>
+            <NameComponentX v-model="person.name" />
+        </div>
+
+        <!-- <div class="">
+            <label for="">Component</label>
+            <NameComponent v-model:fname="fname" v-model:lname="lname" />
+        </div> -->
+
+        <!-- <div class="">
+            <label for="">Name</label>
+            <p>
+                <input type="text" v-model="person.name" />
+            </p>
+        </div> -->
+
+        <!-- <div class="">
+            <label for="">Component</label>
+            <PersonComponent :person="person" />
+        </div> -->
+
+    </div>
+
+</template>
+
+<style scoped></style>
---
+++ multiple-input-in-component-one-v-model/src/components/TextComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+const props = defineProps([
+    'label', 'modelValue'
+]);
+
+const emit = defineEmits(['update:modelValue']);
+
+</script>
+
+<template>
+
+    <p>
+        <label for="">{{ label }}</label> <br />
+        <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" /> <br />
+    </p>
+
+</template>
+
+<style scoped></style>
