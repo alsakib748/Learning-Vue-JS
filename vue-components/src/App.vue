c7710e0b5d5635c5fe46a80cf114dcfa36559f1d
alsakib748 <alsakib748@gmail.com>
2025-07-10 22:49:06 +0600

    Object Component

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -7,6 +7,10 @@ import TextComponents from './components/TextComponents.vue';
 const name = ref('John Doe');
 const email = ref('john@gmail.com');
 
+const person = reactive({
+  name: 'John Doe',
+  email: 'john@doe.com'
+});
 
 </script>
 
@@ -19,30 +23,35 @@ const email = ref('john@gmail.com');
     </div>
 
     <div class="">
-      Original Name: {{ name }}<br />
-      Original Name: {{ email }}
+      Original Name: {{ person.name }}<br />
+      Original Name: {{ person.email }}
     </div>
 
     <div class="">
       <label for="">Name</label>
       <p>
-        <input type="text" v-model="name" class="border border-purple-500 p-2 rounded-lg" />
+        <input type="text" v-model="person.name" class="border border-purple-500 p-2 rounded-lg" />
       </p>
     </div>
 
     <div class="">
       <label for="">Email</label>
       <p>
-        <input type="email" v-model="email" class="border border-purple-500 p-2 rounded-lg" />
+        <input type="email" v-model="person.email" class="border border-purple-500 p-2 rounded-lg" />
       </p>
     </div>
 
     <br />
 
-    <div class="">
+    <!-- <div class="">
       <label for="" style="font-weight: bold;">Component</label><br /><br />
       <TextComponents label="Name" v-model="name" />
       <TextComponents label="Email" v-model="email" />
+    </div> -->
+
+    <div class="">
+      <label for="" style="font-weight: bold;">Component</label><br /><br />
+      <TextComponents :person="person" />
     </div>
 
   </div>
--- vue-components/src/components/TextComponents.vue
+++ vue-components/src/components/TextComponents.vue
@@ -1,20 +1,29 @@
 <script setup>
 
 // v-model == modelValue
+// const props = defineProps([
+//     'label', 'modelValue'
+// ]);
+
 const props = defineProps([
-    'label', 'modelValue'
+    'person'
 ]);
 
-const emit = defineEmits(['update:modelValue']);
+// const emit = defineEmits(['update:modelValue']);
 
 </script>
 
 <template>
 
     <p>
-        <label for="">{{ label }}</label><br />
-        <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
-            class="border border-purple-500 p-2 rounded-lg">
+        <!-- <label for="">{{ label }}</label><br /> -->
+        <!-- <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
+            class="border border-purple-500 p-2 rounded-lg"> -->
+        <label for="">Name</label><br />
+        <input type="text" class="border border-purple-500 p-2 rounded-lg" v-model="person.name" /><br />
+
+        <label for="">Email</label><br />
+        <input type="email" class="border border-purple-500 p-2 rounded-lg" v-model="person.email" /><br />
     </p>
 
 </template>
