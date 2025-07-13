ae3c63c2bdb4c061ef076490ec4665972290a0b4
alsakib748 <alsakib748@gmail.com>
2025-07-13 22:22:54 +0600

    Vue slots

---
+++ vue-slots/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+import { ref, reactive } from 'vue';
+
+import Button from './components/Button.vue';
+
+</script>
+
+<template>
+
+  <div class="">
+
+    <div class="">
+      <img src="./assets/vue.svg" alt="" class="">
+      <h1 class="text-2xl">Vue Slots</h1>
+    </div>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui molestias sit reprehenderit doloremque praesentium
+      fugiat delectus velit dolore. Modi velit officia placeat hic iusto ipsa unde ut cum quibusdam a.
+    </p>
+
+    <br />
+    <br />
+
+    <div class="">
+      <label for="">Component</label>
+      <br /> <br />
+
+      <Button>
+        <template v-slot:licon>
+          <i class="fas fa-home"></i>
+        </template>
+        Don't Click Me
+        <template v-slot:ricon>
+          <i class="fab fa-github"></i>
+        </template>
+      </Button>
+
+      <!-- <Button label="Button 1" />
+      <Button label="Button 2" /> -->
+
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ vue-slots/src/components/Button.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const props = defineProps(['label']);
+
+</script>
+
+<template>
+
+    <button
+        class="my-4 block mx-auto px-3 py-2 bg-purple-500 text-white text-1xl rounded-lg hover:bg-orange-600 hover:text-white">
+        <div class="">
+            <slot name="licon"></slot>
+            <span class="mx-2">
+                <slot>
+                    {{ label }}
+                </slot>
+            </span>
+            <slot name="ricon"></slot>
+        </div>
+    </button>
+
+</template>
+
+<style scoped></style>
